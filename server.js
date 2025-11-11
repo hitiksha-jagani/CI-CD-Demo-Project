2c123baf38bdbed126951e53ca8ecfe677849249
Hitiksha Jagani <hitikshapatel191003@gmail.com>
2025-11-11 18:45:46 +0530

    Some changes

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const client = require("prom-client");
 const app = express();
-const port = process.env.PORT || 3000;
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", client.register.contentType);
+    res.end(await client.register.metrics());
+});
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello from Node.js API!" });
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
-module.exports = app;
+app.listen(3000, () => console.log("Server running on port 3000"));
