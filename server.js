59bf91a951ba23fe22cba7657c47f95fe06e3e55
Hitiksha Jagani <hitikshapatel191003@gmail.com>
2025-11-14 06:59:11 +0530

    Split app and server to fix Jest tests

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-const express = require("express");
-const client = require("prom-client");
-const app = express();
+const app = require("./app");
 
-const collectDefaultMetrics = client.collectDefaultMetrics;
-collectDefaultMetrics();
+const port = 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
-app.get("/metrics", async (req, res) => {
-    res.set("Content-Type", client.register.contentType);
-    res.end(await client.register.metrics());
-});
-
-app.get("/", (req, res) => {
-    res.json({ message: "Hello from Node.js API!" });
-});
-
-app.listen(3000, () => console.log("Server running on port 3000"));
--- test/server.test.js
+++ test/server.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../server");
+const app = require("../app");
 
 describe("GET /", () => {
     it("should return hello message", async () => {
